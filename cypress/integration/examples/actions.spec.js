0df2ba95c536aeece5b11510b99b2da340811a9f
jongregtom <jonthomas83@gmail.com>
2020-04-12 23:43:35 -0700

    test for creating todo, need to verify todo exists

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -10,4 +10,12 @@ context("Actions", () => {
   it("Get header text", () => {
     cy.get(".container > div > h3").should("have.text", "Todos List");
   });
+
+  it("should be able to add a todo", () => {
+    cy.get(".navbar-item").contains("Create Todo").click();
+    cy.get(".form-group").contains("Description: ").parent().find("input").type("first todo");
+    cy.get(".form-group").contains("Responsible: ").parent().find("input").type("responsible");
+    cy.get("#priorityLow").click();
+    cy.get(".form-group").contains("Create Todo").click();
+  })
 });
